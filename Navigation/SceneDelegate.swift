3260adec82a8a5bb19796016cf4a2481f4e78594
Brooklyn125 <mrk_alex@bk.ru>
2024-11-19 09:46:06 +0100

    DZ 2.3.part_1

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Brooklyn on 10/10/2024.
 //
+
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -15,21 +16,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: windowScene)
         
-        let profileVC = ProfileViewController()
-        let profileNavigationController = UINavigationController(rootViewController: profileVC)
-        profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 1)
+        let feedNavController = UINavigationController(rootViewController: FeedViewController())
+        feedNavController.tabBarItem = UITabBarItem(
+            title: "Feed",
+            image: UIImage(systemName: "person.and.background.striped.horizontal"),
+            tag: 0
+        )
+        
+        let logInVC = LogInViewController()
+        let profileNavController = UINavigationController(rootViewController: logInVC)
+        profileNavController.tabBarItem = UITabBarItem(
+            title: "Profile",
+            image: UIImage(systemName: "person.circle"),
+            tag: 1
+        )
         
         let tabBarController = UITabBarController()
-
-        let feedNavController = UINavigationController(rootViewController: FeedViewController())
-        
-        let profileNavController = UINavigationController(rootViewController: ProfileViewController())
-
-        feedNavController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "person.and.background.striped.horizontal"), tag: 0)
-        profileNavController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 1)
-
         tabBarController.viewControllers = [feedNavController, profileNavController]
-
+        
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
     }
